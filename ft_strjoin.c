238719ca4f85b1a8ef7a63f3b61376c2bbaf2456
9dvst1 <damian.cortes@me.com>
2024-06-12 19:16:43 +0200

    feat: add ft_strjoin.c

---
+++ ft_strjoin.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcortes <dcortes@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/15 17:44:59 by dcortes           #+#    #+#             */
+/*   Updated: 2024/06/12 19:15:50 by dcortes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	s1_len;
+	size_t	s2_len;
+	size_t	i;
+	size_t	j;
+	char	*ptr;
+
+	i = 0;
+	j = 0;
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	ptr = malloc((s1_len + s2_len + 1) * sizeof(char));
+	if (!ptr)
+		return (NULL);
+	while (j < s1_len)
+	{
+		ptr[i++] = s1[j++];
+	}
+	j = 0;
+	while (j < s2_len)
+	{
+		ptr[i++] = s2[j++];
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
