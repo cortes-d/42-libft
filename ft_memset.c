c5067a47f50c2d2ee6fec25e405b54ea230b8ce4
9dvst1 <damian.cortes@me.com>
2024-06-12 20:19:02 +0200

    style(ft_memset): minor adjustments

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: dcortes <dcortes@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 13:09:10 by dcortes           #+#    #+#             */
-/*   Updated: 2024/06/12 18:15:33 by dcortes          ###   ########.fr       */
+/*   Updated: 2024/06/12 18:21:42 by dcortes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	*ft_memset(void *b, int c, size_t len)
 	i = 0;
 	while (i < len)
 	{
-		((char *)b)[i] = (unsigned char)c;
+		((unsigned char *)b)[i] = (unsigned char)c;
 		i++;
 	}
 	return (b);
