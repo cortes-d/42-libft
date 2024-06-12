2fd0d3431d1b4d6beab01116f88a10a5d7bec60d
9dvst1 <damian.cortes@me.com>
2024-06-12 19:02:55 +0200

    feat: add ft_atoi.c

---
+++ ft_atoi.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcortes <dcortes@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/10 15:40:21 by dcortes           #+#    #+#             */
+/*   Updated: 2024/06/12 19:02:23 by dcortes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	sign;
+	int	digit;
+	int	number;
+
+	i = 0;
+	sign = 1;
+	digit = 0;
+	number = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] && ft_isdigit(str[i]))
+	{
+		digit = str[i] - '0';
+		number = number * 10 + digit;
+		i++;
+	}
+	return (sign * number);
+}
