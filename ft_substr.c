b203c857f396ad626fadfd82f262e993d8e6b5b0
9dvst1 <damian.cortes@me.com>
2024-06-12 19:12:12 +0200

    feat: add ft_substr.c

---
+++ ft_substr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcortes <dcortes@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/15 17:33:45 by dcortes           #+#    #+#             */
+/*   Updated: 2024/06/12 19:11:30 by dcortes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char			*str;
+	unsigned int	slen;
+	size_t			i;
+
+	slen = (unsigned int) ft_strlen(s);
+	if (start > slen)
+		return (ft_strdup(""));
+	if (len > slen - start)
+		len = slen - start;
+	i = 0;
+	str = malloc((len + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	while (i < len)
+	{
+		str[i] = s[start + i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
