d207461576dbf3c4a1b4a1e96557efce51232bfe
9dvst1 <damian.cortes@me.com>
2024-06-12 18:41:18 +0200

    feat: add ft_strchr.c

---
+++ ft_strchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcortes <dcortes@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/10 14:11:19 by dcortes           #+#    #+#             */
+/*   Updated: 2024/06/12 18:40:52 by dcortes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strchr(const char *s, int c)
+{
+	int				i;
+	unsigned char	cc;
+
+	cc = (unsigned char) c;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == cc)
+			return ((char *) &s[i]);
+		i++;
+	}
+	if (s[i] == cc)
+		return ((char *) &s[i]);
+	return (NULL);
+}
