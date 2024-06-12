3c841a81a6ae5b93fe84d2a9cfb7732100dc6ae1
9dvst1 <damian.cortes@me.com>
2024-06-12 17:51:22 +0200

    feat(header): add prototypes

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dcortes <dcortes@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 17:19:12 by dcortes           #+#    #+#             */
-/*   Updated: 2024/06/12 17:21:16 by dcortes          ###   ########.fr       */
+/*   Updated: 2024/06/12 17:46:05 by dcortes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,14 @@
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
+
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+
+int	ft_islower(int c);
+int	ft_isupper(int c);
+
+int	ft_tolower(int c);
+int	ft_toupper(int c);
 
 #endif
