a302dfab036173ceec7967a5a9a15262266d3480
9dvst1 <damian.cortes@me.com>
2024-06-17 17:30:48 +0200

    style(ft_strtrim): removing empty lines

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: dcortes <dcortes@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 18:42:13 by dcortes           #+#    #+#             */
-/*   Updated: 2024/06/12 19:20:55 by dcortes          ###   ########.fr       */
+/*   Updated: 2024/06/17 17:29:24 by dcortes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,11 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 	if (!s1 || !set)
 		return (NULL);
-
 	start = 0;
 	end = ft_strlen(s1);
-
 	while (s1[start] && ft_strchr(set, s1[start]))
 		start++;
-
 	while (end > start && ft_strchr(set, s1[end - 1]))
 		end--;
-
 	return (ft_substr(s1, start, end - start));
 }
